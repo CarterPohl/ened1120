68828688a1ebfe39297e92f97a6ae99813ab894e
Carter A Pohl <carterpohl7@gmail.com>
2019-02-26 18:18:41 -0500

    Created the file with the header

---
+++ Project4/main.m
@@ -0,0 +1,34 @@
+% Activity XX : 
+% File: 
+% Date: 
+% By:   Jans Tuada
+%       tuadajl
+%       Brent Hull
+%       hullbl
+%       Henry Le
+%       len2
+%       Carter Pohl
+%       pohlca 
+%
+% Section:  001
+% Team:     002
+%
+% ELECTRONIC SIGNATURE
+% Jans Tuada
+% Brent Hull
+% Henry Le
+% Carter Pohl
+%
+% The electronic signatures above indicate the script
+% submitted for evaluation is the combined effort of all
+% team members and that each member of the team was an
+% equal participant in its creation. In addition, each
+% member of the team has a general understanding of all
+% aspect of the script development and execution.
+%
+% A BRIEF DESCRIPTION OF WHAT THE SCRIPT OR FUNCTION DOES
+%
+
+clc
+clear all
+close all
